63f0933d4022f4aa4e9d9600f832cc96cdf022a5
Alebrex99 <ale.brex99@gmail.com>
2024-03-25 18:07:52 +0100

    Aggiunta di scripts utili per cambio scena + interazioni + dotween import

---
+++ Assets/Scripts/cSceneInfo.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class cSceneInfo : MonoBehaviour{
+    public Transform trInitPos;
+    public Transform[] trSlots;
+    public GameObject[] goTeleportPoints;
+    public VideoPlayer vp;
+    public GameObject goVideoPlayer;
+    public GameObject goLogoCentral;
+    public Animator animLogo;
+
+    private bool bShowVideo = false;
+    private float timeLastClick = 0;
+    private void Update() {
+        if (bShowVideo) {
+            Vector3 euler = Quaternion.LookRotation(goVideoPlayer.transform.position - cXRManager.GetTrCenterEye().position).eulerAngles;
+            goVideoPlayer.transform.eulerAngles = new Vector3(0, euler.y, 0);
+        }
+        else {
+            Vector3 euler = Quaternion.LookRotation(goLogoCentral.transform.position - cXRManager.GetTrCenterEye().position).eulerAngles;
+            goLogoCentral.transform.eulerAngles = new Vector3(0, euler.y, 0);
+        }
+    }
+
+    public Transform GetUserInitTr() {
+        return trInitPos;
+    }
+    public Transform GetSlotTr(int id) {
+        trSlots[id].GetChild(0).gameObject.SetActive(true);
+        return trSlots[id];
+    }
+
+    public int GetNumSlots() {
+        return trSlots.Length;
+    }
+
+    public void ShowPath(bool show) {
+        foreach(GameObject go in goTeleportPoints)
+            go.SetActive(show);
+    }
+    public void ClickLogo() {
+        Debug.Log("Tocado logo");
+        animLogo.ResetTrigger("HideVideo");
+        animLogo.SetTrigger("ShowVideo");
+        vp.loopPointReached += EndVideo;
+        vp.isLooping = true;
+        vp.Play();
+        bShowVideo = true;
+    }
+    public void ClickButtonVideo() {
+        if (Time.realtimeSinceStartup - timeLastClick < 1) {
+            return;
+        }
+        if (bShowVideo) {
+            vp.loopPointReached -= EndVideo;
+            vp.Stop();
+            animLogo.ResetTrigger("ShowVideo");
+            animLogo.SetTrigger("HideVideo");
+            bShowVideo = false;
+            timeLastClick = Time.realtimeSinceStartup;
+        }
+        else {
+            Debug.Log("Tocado logo");
+            animLogo.ResetTrigger("HideVideo");
+            animLogo.SetTrigger("ShowVideo");
+            vp.loopPointReached += EndVideo;
+            vp.isLooping = true;
+            vp.Play();
+            bShowVideo = true;
+            timeLastClick = Time.realtimeSinceStartup;
+            //cDataManager.AddResponse(eDataSesionAction.VIDEO, "");
+        }
+    }
+    public void SetVideo(VideoClip vc) {
+        vp.clip = vc;
+    }
+
+    private void EndVideo(VideoPlayer vp) {
+        vp.loopPointReached -= EndVideo;
+        vp.Stop();
+        animLogo.ResetTrigger("ShowVideo");
+        animLogo.SetTrigger("HideVideo");
+        bShowVideo = false;
+    }
+
+}
---
+++ Assets/Scripts/cXRManager.cs
@@ -0,0 +1,74 @@
+using Oculus.Interaction.Input;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class cXRManager : MonoBehaviour{
+    public Hand leftHand;
+    public Hand rightHand;
+    public Transform trCenterEye;
+    public Transform trUserPosition;
+    public Transform trMunyecaLeft;
+    public Transform trMunyecaRight;
+    public Transform trLeftHand;
+    public Transform trRightHand;
+
+    private static cXRManager instance;
+    private static UnityEvent poseSelectLeftPaper;
+    private static UnityEvent poseSelectRightPaper;
+    private static UnityEvent poseSelectRightPistol;
+    private static UnityEvent poseSelectLeftPistol;
+    private static UnityEvent poseUnselectLeftPaper;
+    private static UnityEvent poseUnselectRightPaper;
+    private static UnityEvent poseUnselectRightPistol;
+    private static UnityEvent poseUnselectLeftPistol;
+
+    private void Awake() {
+        instance = this;
+        poseSelectLeftPaper = new UnityEvent();
+        poseSelectRightPaper = new UnityEvent();
+        poseSelectRightPistol = new UnityEvent();
+        poseSelectLeftPistol = new UnityEvent();
+        poseUnselectLeftPaper = new UnityEvent();
+        poseUnselectRightPaper = new UnityEvent();
+        poseUnselectRightPistol = new UnityEvent();
+        poseUnselectLeftPistol = new UnityEvent();
+    }
+    public static Hand GetLeftHand() { return instance.leftHand; }
+    public static Hand GetRighttHand() { return instance.rightHand; }
+    public static Transform GetTrCenterEye() { return instance.trCenterEye; }
+    public static Transform GetTrMunyecaLeft() { return instance.trMunyecaLeft; }
+    public static Transform GetTrMunyecaRight() { return instance.trMunyecaRight; }
+    public static Transform GetTrLeftHand() { return instance.trLeftHand; }
+    public static Transform GetTrRightHand() { return instance.trRightHand; }
+
+    public static void SetUserPosition(Vector3 position, Quaternion rotation) {
+        instance.trUserPosition.position = position;
+        instance.trUserPosition.rotation = rotation;
+    }
+
+    public void OnPoseSelectLeftPaper() { poseSelectLeftPaper?.Invoke(); }
+    public void OnPoseSelectRightPaper() { poseSelectRightPaper?.Invoke(); }
+    public void OnPoseSelectRightPistol() { poseSelectRightPistol?.Invoke(); }
+    public void OnPoseSelectLeftPistol() { poseSelectLeftPistol?.Invoke(); }
+    public void OnPoseUnselectLeftPaper() { poseUnselectLeftPaper?.Invoke(); }
+    public void OnPoseUnselectRightPaper() { poseUnselectRightPaper?.Invoke(); }
+    public void OnPoseUnselectRightPistol() { poseUnselectRightPistol?.Invoke(); }
+    public void OnPoseUnselectLeftPistol() { poseUnselectLeftPistol?.Invoke(); }
+    public static void AddListenerPoseSelectLeftPaper(UnityAction call) { poseSelectLeftPaper.AddListener(call);}
+    public static void AddListenerPoseSelectRightPaper(UnityAction call) { poseSelectRightPaper.AddListener(call); }
+    public static void AddListenerPoseSelectRightPistol(UnityAction call) { poseSelectRightPistol.AddListener(call); }
+    public static void AddListenerPoseSelectLeftPistol(UnityAction call) { poseSelectLeftPistol.AddListener(call); }
+    public static void AddListenerPoseUnselectLeftPaper(UnityAction call) { poseUnselectLeftPaper.AddListener(call); }
+    public static void AddListenerPoseUnselectRightPaper(UnityAction call) { poseUnselectRightPaper.AddListener(call); }
+    public static void AddListenerPoseUnselectRightPistol(UnityAction call) { poseUnselectRightPistol.AddListener(call); }
+    public static void AddListenerPoseUnselectLeftPistol(UnityAction call) { poseUnselectLeftPistol.AddListener(call); }
+    public static void RemoveListenerPoseSelectLeftPaper(UnityAction call) { poseSelectLeftPaper.RemoveListener(call);}
+    public static void RemoveListenerPoseSelectRightPaper(UnityAction call) { poseSelectRightPaper.RemoveListener(call); }
+    public static void RemoveListenerPoseSelectRightPistol(UnityAction call) { poseSelectRightPistol.RemoveListener(call); }
+    public static void RemoveListenerPoseSelectLeftPistol(UnityAction call) { poseSelectLeftPistol.RemoveListener(call); }
+    public static void RemoveListenerPoseUnselectLeftPaper(UnityAction call) { poseUnselectLeftPaper.RemoveListener(call); }
+    public static void RemoveListenerPoseUnselectRightPaper(UnityAction call) { poseUnselectRightPaper.RemoveListener(call); }
+    public static void RemoveListenerPoseUnselectRightPistol(UnityAction call) { poseUnselectRightPistol.RemoveListener(call); }
+    public static void RemoveListenerPoseUnselectLeftPistol(UnityAction call) { poseUnselectLeftPistol.RemoveListener(call); }
+
+}
