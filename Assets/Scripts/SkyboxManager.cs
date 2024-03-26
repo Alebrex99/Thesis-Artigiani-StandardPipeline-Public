21e2be71bc3e77157b03d5deb3a6a5c2ae5d638c
Alebrex99 <ale.brex99@gmail.com>
2024-03-26 12:48:44 +0100

    After Jesus tips : setting prefabs+ Models
    guardare appunti su fogli: linea guida per gestione packages

---
+++ Assets/Scripts/SkyboxManager.cs
@@ -0,0 +1,36 @@
+using Meta.WitAi;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxManager: MonoBehaviour
+{
+    [SerializeField] Material skybox1;
+    [SerializeField] Material skybox2;
+    //[SerializeField] Material skybox3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnButton1Pressed()
+    {       
+        if(RenderSettings.skybox == skybox1)
+        {
+            RenderSettings.skybox = skybox2;
+        }
+        else
+        {
+            RenderSettings.skybox = skybox1;
+        }
+    }
+        
+}
