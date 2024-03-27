4c4d12e873318d216c409a35bd413501a92593c5
Alebrex99 <ale.brex99@gmail.com>
2024-03-27 15:41:38 +0100

    OnButtonchangeScene() implementation start

--- Assets/Scripts/SceneTransition/SceneTransitionManager.cs
+++ Assets/Scripts/SceneTransition/SceneTransitionManager.cs
@@ -11,11 +11,68 @@ public class SceneTransitionManager : MonoBehaviour
 
     //Scene transition
     [SerializeField] private LoadSceneMode _loadSceneMode;
-    private AsyncOperation asyncLoadOperation; //restituito dalle operazioni asincrone per determinare se op è completata
+    [SerializeField] private string _sceneToLoad;
+    [SerializeField] private string _sceneToUnload;
+    [SerializeField] private bool _unloadSceneOnExit;
+    private bool _buttonExit = false;
+    private AsyncOperation asyncLoadOperation; //restituito dalle operazioni asincrone per determinare se op è completata, usabile per progress bar
 
-    
-  
-    
+
+    public void onButtonChangeScene()
+    {
+        //premi il bottone -> carica scena office + scarica environemnt
+        if (_buttonExit) //sono in uscita dall'ufficio
+        {
+            if (!_unloadSceneOnExit)
+                return;
+            Scene sceneToUnload = SceneManager.GetSceneByName(_sceneToUnload);
+            if (!sceneToUnload.IsValid() || !sceneToUnload.isLoaded)
+                return;
+
+            StartCoroutine(UnloadScene(sceneToUnload));
+            
+        }
+        else //sono in entrata nell'ufficio
+        {
+            StartCoroutine(LoadScene(_sceneToLoad));
+            _buttonExit = true;
+        }
+
+
+        //premi ancora il bottone -> carica scena environment + scarica office
+    }
+
+    private IEnumerator LoadScene(string sceneToLoadName)
+    {
+        Scene sceneToLoad = SceneManager.GetSceneByName(sceneToLoadName);
+        if (sceneToLoad.IsValid() && sceneToLoad.isLoaded)
+            yield break;
+
+        asyncLoadOperation = SceneManager.LoadSceneAsync(sceneToLoadName, _loadSceneMode);
+        while (!asyncLoadOperation.isDone)
+        {
+            yield return null;
+        }
+        
+        asyncLoadOperation = null;
+        Scene loadedScene = SceneManager.GetSceneByName(sceneToLoadName);
+        SceneManager.SetActiveScene(loadedScene);
+
+    }
+
+    private IEnumerator UnloadScene(Scene sceneToUnload)
+    {
+        while (asyncLoadOperation != null && !asyncLoadOperation.isDone)
+        {
+            yield return null;
+        }
+
+        SceneManager.UnloadSceneAsync(sceneToUnload);
+    }
+
+
+
+
 
     //FUNZIONI PER CAMBIO SCENA ESPOSTE:
     //unloading scene environment per eliminare l'ambiente base quando premo pulsante
