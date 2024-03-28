e980779a59d3d86dad97a8b090dda5e7365c6d1c
Alebrex99 <ale.brex99@gmail.com>
2024-03-28 11:34:46 +0100

    following jesus tip (1 scene) + add all buttons + start button4 180 vid

--- Assets/Scripts/SkyboxManager.cs
+++ Assets/Scripts/SkyboxManager.cs
@@ -8,14 +8,26 @@ public class SkyboxManager: MonoBehaviour
     [SerializeField] Material skyboxImg360Mono;
     [SerializeField] Material skyboxImg180Mono;
     [SerializeField] Material skyboxBase;
+    [SerializeField] GameObject _environement;
+    [SerializeField] GameObject _office;
     public FadeScreen fadeScreen;
+    
 
     private void Start()
     {
         RenderSettings.skybox = skyboxBase;
     }
     public void OnButton1Pressed()
-    {       
+    {   /*  
+        if (_environement != null)
+        {
+            _environement.SetActive(true);
+        }
+        if (_office != null)
+        {
+            _office.SetActive(false);
+        }
+        */
         if(RenderSettings.skybox == skyboxImg360Mono)
         {
             RenderSettings.skybox = skyboxBase;
@@ -24,6 +36,11 @@ public class SkyboxManager: MonoBehaviour
         {   
             RenderSettings.skybox = skyboxImg360Mono;
         }
+    }
+
+    public void OnButton3Pressed()
+    {
+
     }
         
 }
